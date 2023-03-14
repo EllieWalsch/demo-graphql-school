cd24e113205b309931a0f9e1a371c0dd4e00674c
EllieWalsch <elliewalsch4@gmail.com>
2023-03-13 19:43:53 -0500

    Add classwork

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -1,10 +1,18 @@
 import classController from "../class/controller.js";
+import professorController from "../professor/controller.js";
+import schoolController from "../school/controller.js";
 
 const resolvers = {
   Query: {
     async indexClasses() {
       // cannot return a promise in a resolver, must include the await
       return await classController.index();
+    },
+    async indexSchools() {
+      return await schoolController.index();
+    },
+    async indexProfessors() {
+      return await professorController.index();
     },
   },
 };
--- app/graphql/typedefs.js
+++ app/graphql/typedefs.js
@@ -4,6 +4,8 @@ const typeDefs = gql`
   type Query {
     "Get all the classes. if there are none, return an empty array"
     indexClasses: [Class]!
+    indexSchools: [School!]!
+    indexProfessors: [Professor!]!
   }
 
   type Class {
@@ -13,6 +15,22 @@ const typeDefs = gql`
     name: String!
     building: String!
     creditHours: Int!
+  }
+
+  type School {
+    _id: ID!
+    name: String!
+    location: String!
+    studentCount: Int!
+    classes: [Class]
+  }
+
+  type Professor {
+    _id: ID!
+    name: String!
+    studentScore: Float!
+    officeHours: String!
+    officeLocation: String!
   }
 `;
 
